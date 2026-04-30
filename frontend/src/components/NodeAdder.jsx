651c148af12b5d75902abfb029c1ef2790588f50
zt81 <zafertunamercanoglu@gmail.com>
2026-04-30 12:34:01 +0300

    Add NodeAdder component

---
+++ frontend/src/components/NodeAdder.jsx
@@ -0,0 +1,10 @@
+function NodeAdder() {
+  return (
+    <section>
+      <h2>Node Adder</h2>
+      <p>Yeni PCB bileşeni ekleme arayüzü burada geliştirilecektir.</p>
+    </section>
+  );
+}
+
+export default NodeAdder;
